5bb10494165c2db1e8bc8fac43f973f119ce332b
Mitchell Clode <mitchell@mitchdev.com>
2011-09-21 23:54:40 +0930

    + Infinite Pistons can now spawn a sequence of blocks.

--- src/mitch/infpist/IPBlockListener.java
+++ src/mitch/infpist/IPBlockListener.java
@@ -1,5 +1,8 @@
 package mitch.infpist;
 
+import java.util.HashMap;
+import java.util.List;
+
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.Sign;
@@ -16,9 +19,11 @@ import org.bukkit.material.MaterialData;
 
 public class IPBlockListener extends BlockListener {
     private final InfinitePiston plugin;
+    private HashMap<Block, Integer> index;
 
     public IPBlockListener(final InfinitePiston plugin) {
         this.plugin = plugin;
+        this.index = new HashMap<Block, Integer>();
     }
     
     private Block getAttachedBlock(Block b) {
@@ -34,10 +39,35 @@ public class IPBlockListener extends BlockListener {
 		Sign sign = (Sign)event.getBlock().getRelative(face).getState();
 		if (sign.getLine(0).equals("[InfPist]")) {
 			String blockID = sign.getLine(1);
-			if (Material.matchMaterial(blockID) != null) {
-				Block b = event.getBlock().getRelative(event.getDirection());
-				if (b.getType() == Material.AIR) {
-					b.setType(Material.matchMaterial(blockID));
+			String[] chunks = blockID.split(",");
+			if (chunks.length > 1) {
+				String l = sign.getLine(2);
+				int i = 0;
+				if (!this.index.containsKey(event.getBlock())) {
+					System.out.println ("No in hashmap");
+					this.index.put(event.getBlock(), 0);
+				}
+				else {
+					i = this.index.get(event.getBlock());
+					i++;
+					if (i >= chunks.length) {
+						i = 0;
+					}
+					this.index.put(event.getBlock(), i);
+				}
+				if (Material.matchMaterial(chunks[i]) != null) {
+					Block b = event.getBlock().getRelative(event.getDirection());
+					if (b.getType() == Material.AIR) {
+						b.setType(Material.matchMaterial(chunks[i]));
+					}
+				}
+			}
+			else {
+				if (Material.matchMaterial(blockID) != null) {
+					Block b = event.getBlock().getRelative(event.getDirection());
+					if (b.getType() == Material.AIR) {
+						b.setType(Material.matchMaterial(blockID));
+					}
 				}
 			}
 		}
